c9a41a7600ee6d4f3035f1160bf351dd43a13720
iamsourav37 <souravganguly108@gmail.com>
2022-01-03 21:13:46 +0530

    Category model is defined

--- backend/api/category/models.py
+++ backend/api/category/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.CharField(max_length=300, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return f"{self.id} ---> {self.name}"
--- backend/ecom/settings.py
+++ backend/ecom/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'api.apps.ApiConfig',
+    'api.category.apps.CategoryConfig'
 ]
 
 MIDDLEWARE = [
