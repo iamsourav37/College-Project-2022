7b7e41473a145eba417f1719796d99c5b04dafd4
iamsourav37 <souravganguly108@gmail.com>
2022-01-10 23:06:11 +0530

    user model is implemented

--- backend/api/user/models.py
+++ backend/api/user/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50, default="Anonomous")
+    email = models.EmailField(null=False, blank=False, unique=True)
+    username = None
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    phoneno = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    address = models.CharField(max_length=1000, blank=True, null=True)
+
+    session_token = models.CharField(max_length=10, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
